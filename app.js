f5a4f7378a3ff08744eeb28908cabdc52e452c32
ninzyadityawidi <ninzyadityawidi@gmail.com>
2023-01-12 01:27:35 +0700

    Menambahkan tools compression

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const compression = require('compression')
 
 const homeRouter = require('./routes/home')
 const loginRouter = require('./routes/login')
@@ -29,6 +30,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(compression())
 
 app.use('/', (req, res, next) => {
 	req.currentPage = req.originalUrl.split('/')[1]
