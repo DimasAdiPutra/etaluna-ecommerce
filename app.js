4776848f37600c8ba9895f4eca2b4b8d85c1406b
ninzyadityawidi <ninzyadityawidi@gmail.com>
2023-01-12 01:53:37 +0700

    Menghilangkan tools compression

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(compression())
 
 app.use('/', (req, res, next) => {
 	req.currentPage = req.originalUrl.split('/')[1]
