aeb5f7ab338c86459d20e05a9bfa373a61e44ac2
DimasAdiPutra <dimasadiputra528@gmail.com>
2023-06-26 22:10:16 +0700

    Membuat footer dan mengganti feather icon dengan font awesome

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ require.context('./images', true)
 require('./styles/tailwind.css')
 
 //* require js
-/**
- * @import feather-icons
- */
-const feather = require('feather-icons')
-
-// Menjalankan feather icons
-feather.replace({
-	class: 'icons',
-	// color: '#1B1B1B',
-	// width: '16px',
-})
-
 require('./javascripts/nav')
 require('./javascripts/dialog')
 // require('./javascripts/form')
